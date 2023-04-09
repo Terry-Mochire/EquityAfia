bd1517589daa86e6555c7dfe24455248a0feb123
Terry Mochire <terrybmochire@gmail.com>
2023-04-09 18:14:39 +0300

    Refactor UIs

--- app/src/main/java/com/hackathon/equityafia/feature_auth/presentation/SignInScreen.kt
+++ app/src/main/java/com/hackathon/equityafia/feature_auth/presentation/SignInScreen.kt
@@ -3,10 +3,7 @@ package com.hackathon.equityafia.feature_auth.presentation
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -21,78 +18,89 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.hackathon.equityafia.R
+import com.hackathon.equityafia.ui.theme.EquityAfiaTheme
 
 @Composable
-fun SignInScreen() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        ClickableText(
-            text = AnnotatedString("Forgot password?"),
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(20.dp),
-            onClick = { },
-            style = TextStyle(
-                fontSize = 14.sp,
-                fontFamily = SansSerif,
-                textDecoration = TextDecoration.Underline,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-        )
-    }
-    Column(
-        modifier = Modifier.padding(20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-
-        val username = remember { mutableStateOf(TextFieldValue()) }
-        val password = remember { mutableStateOf(TextFieldValue()) }
-
-        OutlinedTextField(
-            value = username.value,
-            onValueChange = { username.value = it },
-            label = { Text("Username") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
-        )
-
-        OutlinedTextField(
-            value = password.value,
-            onValueChange = { password.value = it },
-            label = { Text("Password") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
-        )
-
-        Button(
-            onClick = { /*TODO*/ }
+fun SignInScreen(navController: NavHostController) {
+    EquityAfiaTheme() {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
         ) {
-            Text("Sign in")
-        }
-
-        Text(text = "Or sign with", modifier = Modifier.padding(10.dp))
-
-        Button(
-            onClick = { /*TODO*/ }
-        ) {
-            Row {
-                Image(
-                    painter = painterResource(id = R.drawable.google),
-                    contentDescription = "",
+            Box(modifier = Modifier.fillMaxSize()) {
+                ClickableText(
+                    text = AnnotatedString("Forgot password?"),
                     modifier = Modifier
-                        .size(20.dp)
-                        .align(Alignment.CenterVertically)
+                        .align(Alignment.BottomCenter)
+                        .padding(20.dp),
+                    onClick = { },
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = SansSerif,
+                        textDecoration = TextDecoration.Underline,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                )
+            }
+            Column(
+                modifier = Modifier.padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+
+                val username = remember { mutableStateOf(TextFieldValue()) }
+                val password = remember { mutableStateOf(TextFieldValue()) }
+
+                OutlinedTextField(
+                    value = username.value,
+                    onValueChange = { username.value = it },
+                    label = { Text("Username") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp)
                 )
 
-                Text(
-                    text = "Google",
-                    modifier = Modifier.padding(start = 10.dp)
+                OutlinedTextField(
+                    value = password.value,
+                    onValueChange = { password.value = it },
+                    label = { Text("Password") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp)
                 )
+
+                Button(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text("Sign in")
+                }
+
+                Text(text = "Or sign with", modifier = Modifier.padding(10.dp))
+
+                Button(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Row {
+                        Image(
+                            painter = painterResource(id = R.drawable.google),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .size(20.dp)
+                                .align(Alignment.CenterVertically)
+                        )
+
+                        Text(
+                            text = "Google",
+                            modifier = Modifier.padding(start = 10.dp)
+                        )
+
+                    }
+                }
 
             }
+
         }
 
     }
@@ -102,5 +110,6 @@ fun SignInScreen() {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun SignInScreenPreview() {
-    SignInScreen()
+    val navController = rememberNavController()
+    SignInScreen(navController)
 }
--- app/src/main/java/com/hackathon/equityafia/feature_auth/presentation/SignUpScreen.kt
+++ app/src/main/java/com/hackathon/equityafia/feature_auth/presentation/SignUpScreen.kt
@@ -1,13 +1,9 @@
 package com.hackathon.equityafia.feature_auth.presentation
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -22,98 +18,116 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.hackathon.equityafia.R
+import com.hackathon.equityafia.feature_navigation.Screens
+import com.hackathon.equityafia.ui.theme.EquityAfiaTheme
 
 @Composable
-fun SignUpScreen() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        ClickableText(
-            text = AnnotatedString("Already have an account? Sign in"),
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(20.dp),
-            onClick = { /*TODO*/ },
-            style = TextStyle(
-                fontSize = 14.sp,
-                fontFamily = FontFamily.SansSerif,
-                textDecoration = TextDecoration.Underline,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-        )
-    }
-
-    Column(
-        modifier = Modifier.padding(20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        val newUserName = remember { mutableStateOf(TextFieldValue()) }
-        val newPassword = remember { mutableStateOf(TextFieldValue()) }
-        val confirmPassword = remember { mutableStateOf(TextFieldValue()) }
-        val email = remember { mutableStateOf(TextFieldValue()) }
-
-        OutlinedTextField(
-            value = email.value,
-            onValueChange = { email.value = it },
-            label = { Text("Email") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
-        )
-
-        OutlinedTextField(
-            value = newUserName.value,
-            onValueChange = { newUserName.value = it },
-            label = { Text("Username") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
-        )
-
-        OutlinedTextField(
-            value = newPassword.value,
-            onValueChange = { newPassword.value = it },
-            label = { Text("Password") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
-        )
-
-        OutlinedTextField(
-            value = confirmPassword.value,
-            onValueChange = { confirmPassword.value = it },
-            label = { Text("Confirm Password") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
-        )
-
-        Button(onClick = { /*TODO*/ }) {
-            Text("Sign Up")
-        }
-
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(text = "Or Sign Up with")
-
-        Button(onClick = { /*TODO*/ }) {
-            Row {
-                Image(
-                    painter = painterResource(id = R.drawable.google),
-                    contentDescription = "",
+fun SignUpScreen(navController: NavHostController) {
+    EquityAfiaTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                ClickableText(
+                    text = AnnotatedString("Already have an account? Sign in"),
                     modifier = Modifier
-                        .size(20.dp)
-                        .align(Alignment.CenterVertically)
+                        .align(Alignment.BottomCenter)
+                        .padding(20.dp),
+                    onClick = {
+                        navController.navigate(Screens.SignInScreen.route){
+                            popUpTo(Screens.SignUpScreen.route){
+                                inclusive = true
+                            }
+                        }
+                    },
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = FontFamily.SansSerif,
+                        textDecoration = TextDecoration.Underline,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                )
+            }
+
+            Column(
+                modifier = Modifier.padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                val newUserName = remember { mutableStateOf(TextFieldValue()) }
+                val newPassword = remember { mutableStateOf(TextFieldValue()) }
+                val confirmPassword = remember { mutableStateOf(TextFieldValue()) }
+                val email = remember { mutableStateOf(TextFieldValue()) }
+
+                OutlinedTextField(
+                    value = email.value,
+                    onValueChange = { email.value = it },
+                    label = { Text("Email") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp)
                 )
 
-                Text(
-                    text = "Google",
-                    modifier = Modifier.padding(start = 10.dp)
+                OutlinedTextField(
+                    value = newUserName.value,
+                    onValueChange = { newUserName.value = it },
+                    label = { Text("Username") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp)
                 )
+
+                OutlinedTextField(
+                    value = newPassword.value,
+                    onValueChange = { newPassword.value = it },
+                    label = { Text("Password") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp)
+                )
+
+                OutlinedTextField(
+                    value = confirmPassword.value,
+                    onValueChange = { confirmPassword.value = it },
+                    label = { Text("Confirm Password") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp)
+                )
+
+                Button(onClick = { /*TODO*/ }) {
+                    Text("Sign Up")
+                }
+
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(text = "Or Sign Up with")
+
+                Button(onClick = { /*TODO*/ }) {
+                    Row {
+                        Image(
+                            painter = painterResource(id = R.drawable.google),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .size(20.dp)
+                                .align(Alignment.CenterVertically)
+                        )
+
+                        Text(
+                            text = "Google",
+                            modifier = Modifier.padding(start = 10.dp)
+                        )
+
+                    }
+
+                }
+
 
             }
 
         }
-
 
     }
 
@@ -122,5 +136,6 @@ fun SignUpScreen() {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun SignUpScreenPreview() {
-    SignUpScreen()
+    val navController = rememberNavController()
+    SignUpScreen(navController)
 }
