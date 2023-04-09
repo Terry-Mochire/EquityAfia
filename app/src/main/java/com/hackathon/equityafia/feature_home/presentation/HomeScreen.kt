551a26b8bfb27cc6b39f655ef9128860cf0e6e71
Terry Mochire <terrybmochire@gmail.com>
2023-04-09 18:15:47 +0300

    Create the home screen of the application.

---
+++ app/src/main/java/com/hackathon/equityafia/feature_home/presentation/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.hackathon.equityafia.feature_home.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+
+}
