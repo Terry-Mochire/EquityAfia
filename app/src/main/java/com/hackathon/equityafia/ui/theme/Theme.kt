e69beb0f2ea1f35773d2812bd75a5feb539202cf
Terry Mochire <terrybmochire@gmail.com>
2023-04-09 18:14:01 +0300

    Refactor theme to include dynamic theme by Material 3.

--- app/src/main/java/com/hackathon/equityafia/ui/theme/Theme.kt
+++ app/src/main/java/com/hackathon/equityafia/ui/theme/Theme.kt
@@ -1,10 +1,15 @@
 package com.hackathon.equityafia.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
 
 
 private val LightColors = lightColorScheme(
@@ -69,17 +74,30 @@ private val DarkColors = darkColorScheme(
 
 @Composable
 fun EquityAfiaTheme(
-  useDarkTheme: Boolean = isSystemInDarkTheme(),
+  darkTheme: Boolean = isSystemInDarkTheme(),
+  dynamicColor: Boolean = true,
   content: @Composable() () -> Unit
-) {
-  val colors = if (!useDarkTheme) {
-    LightColors
-  } else {
-    DarkColors
-  }
 
-  MaterialTheme(
-    colorScheme = colors,
-    content = content
-  )
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColors
+        else -> LightColors
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
 }
