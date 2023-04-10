ce51aded7e9e5c261d8fb4adc5a55c78f56f44be
Terry Mochire <terrybmochire@gmail.com>
2023-04-11 00:58:58 +0300

    Add methods to create CRSF token and get clinic by name.

--- app/src/main/java/com/hackathon/equityafia/feature_clinics/data/repository/ApiRepository.kt
+++ app/src/main/java/com/hackathon/equityafia/feature_clinics/data/repository/ApiRepository.kt
@@ -4,15 +4,29 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import com.hackathon.equityafia.feature_clinics.data.remote.api.ApiService
 import com.hackathon.equityafia.feature_clinics.data.remote.models.responses.AllClinicsResponseItem
-import com.hackathon.equityafia.feature_clinics.data.remote.models.responses.ClinicResponseItem
 import javax.inject.Inject
 
 class ApiRepository @Inject constructor(private val apiService: ApiService) {
 
     val clinics: MutableState<List<AllClinicsResponseItem>> = mutableStateOf(emptyList())
 
+    private var token:String? = null
+
+    private suspend fun getToken(): String {
+        if (this.token == null) {
+            this.token = apiService.getCsrfToken().csrf_token
+        }
+        return this.token!!
+    }
+
+
     suspend fun getAllClinics(){
         val response = apiService.getAllClinics()
+        clinics.value = response
+    }
+
+    suspend fun getClinic(name: String) {
+       val response = apiService.getClinicByName(getToken(), name)
         clinics.value = response
     }
 
