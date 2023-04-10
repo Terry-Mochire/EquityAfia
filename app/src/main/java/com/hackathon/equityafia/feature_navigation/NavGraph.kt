07da249b02d38e5e6941d6f7e40ac9aff256bc8e
Terry Mochire <terrybmochire@gmail.com>
2023-04-10 20:27:55 +0300

    Add required parameter.

--- app/src/main/java/com/hackathon/equityafia/feature_navigation/NavGraph.kt
+++ app/src/main/java/com/hackathon/equityafia/feature_navigation/NavGraph.kt
@@ -4,10 +4,16 @@ import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.composable
 import com.hackathon.equityafia.feature_auth.presentation.*
+import com.hackathon.equityafia.feature_clinics.data.remote.api.ApiService
+import com.hackathon.equityafia.feature_clinics.data.repository.ApiRepository
+import com.hackathon.equityafia.feature_clinics.di.ApiModule
+import com.hackathon.equityafia.feature_clinics.presentation.ClinicsViewModel
 import com.hackathon.equityafia.feature_home.presentation.HomeScreen
 
 
@@ -25,6 +31,10 @@ fun NavGraph(
 
             val authRepository = AuthRepository()
             val authViewModel = AuthViewModel(authRepository = authRepository)
+            val httpLoggingInterceptor = ApiModule.providesLoggingInterceptor()
+            val okHttpClient = ApiModule.providesOkHttpClient(httpLoggingInterceptor)
+            val apiService = ApiModule.providesAPIService(okHttpClient)
+            val clinicsViewModel = ClinicsViewModel(repository = ApiRepository(apiService = apiService))
 
             composable(Screens.SignInScreen.route){
                 SignInScreen(navController = navController, repository = authRepository ,viewModel = authViewModel)
@@ -33,7 +43,7 @@ fun NavGraph(
                 SignUpScreen(navController = navController, repository = authRepository ,viewModel = authViewModel)
             }
             composable(Screens.HomeScreen.route){
-                HomeScreen(navController = navController, viewModel = authViewModel)
+                HomeScreen(navController = navController, viewModel = authViewModel, clinicsViewModel = clinicsViewModel)
             }
 
             composable(Screens.ProfileScreen.route){
