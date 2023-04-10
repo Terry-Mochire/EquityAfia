0af6fe85b8d77c7f2d194ce4919c9a1783c622bf
Terry Mochire <terrybmochire@gmail.com>
2023-04-11 01:02:40 +0300

    Add methods to getClinic by name.

--- app/src/main/java/com/hackathon/equityafia/feature_clinics/presentation/viewmodels/ClinicsViewModel.kt
+++ app/src/main/java/com/hackathon/equityafia/feature_clinics/presentation/viewmodels/ClinicsViewModel.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.MutableState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hackathon.equityafia.feature_clinics.data.remote.models.responses.AllClinicsResponseItem
-import com.hackathon.equityafia.feature_clinics.data.remote.models.responses.ClinicResponseItem
 import com.hackathon.equityafia.feature_clinics.data.repository.ApiRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -16,8 +15,13 @@ class ClinicsViewModel @Inject constructor(
     private val repository: ApiRepository
 ) : ViewModel() {
 
-    val clinics: MutableState<List<AllClinicsResponseItem>> = repository.clinics
+    var clinics: MutableState<List<AllClinicsResponseItem>> = repository.clinics
 
+    fun getClinic(name: String){
+        viewModelScope.launch {
+             repository.getClinic(name)
+        }
+    }
 
     init {
        viewModelScope.launch {
