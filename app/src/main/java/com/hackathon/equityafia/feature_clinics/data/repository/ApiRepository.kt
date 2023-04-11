8c856d07be754e4aaca2ee6504afa3d8343d9620
Terry Mochire <terrybmochire@gmail.com>
2023-04-11 03:20:19 +0300

    Create function to retrieve the services in a clinic.

--- app/src/main/java/com/hackathon/equityafia/feature_clinics/data/repository/ApiRepository.kt
+++ app/src/main/java/com/hackathon/equityafia/feature_clinics/data/repository/ApiRepository.kt
@@ -4,11 +4,13 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import com.hackathon.equityafia.feature_clinics.data.remote.api.ApiService
 import com.hackathon.equityafia.feature_clinics.data.remote.models.responses.AllClinicsResponseItem
+import com.hackathon.equityafia.feature_clinics.data.remote.models.responses.AllServicesResponseItem
 import javax.inject.Inject
 
 class ApiRepository @Inject constructor(private val apiService: ApiService) {
 
     val clinics: MutableState<List<AllClinicsResponseItem>> = mutableStateOf(emptyList())
+    val services: MutableState<List<AllServicesResponseItem>> = mutableStateOf(emptyList())
 
     private var token:String? = null
 
@@ -28,6 +30,11 @@ class ApiRepository @Inject constructor(private val apiService: ApiService) {
     suspend fun getClinic(name: String) {
        val response = apiService.getClinicByName(getToken(), name)
         clinics.value = response
+    }
+
+    suspend fun getSerivicesInAClinic(id: Int) {
+       val response = apiService.getAllServicesInAClinic(id)
+        services.value = response
     }
 
 
