8b9590237833880c2d47c2f283310a4c8ff3dcde
Terry Mochire <terrybmochire@gmail.com>
2023-04-11 04:59:11 +0300

    Implement methods to get locations and clinics in a specific location.

--- app/src/main/java/com/hackathon/equityafia/feature_clinics/presentation/viewmodels/ClinicsViewModel.kt
+++ app/src/main/java/com/hackathon/equityafia/feature_clinics/presentation/viewmodels/ClinicsViewModel.kt
@@ -1,10 +1,11 @@
 package com.hackathon.equityafia.feature_clinics.presentation.viewmodels
 
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.hackathon.equityafia.feature_clinics.data.remote.models.responses.AllClinicsResponseItem
-import com.hackathon.equityafia.feature_clinics.data.remote.models.responses.AllServicesResponseItem
+import com.hackathon.equityafia.feature_clinics.data.remote.models.requests.Location
+import com.hackathon.equityafia.feature_clinics.data.remote.models.responses.*
 import com.hackathon.equityafia.feature_clinics.data.repository.ApiRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -18,6 +19,9 @@ class ClinicsViewModel @Inject constructor(
 
     var clinics: MutableState<List<AllClinicsResponseItem>> = repository.clinics
     var services: MutableState<List<AllServicesResponseItem>> = repository.services
+    var countries: MutableState<List<AllCountriesResponseItem>> = repository.countries
+    var counties: MutableState<List<AllCountiesResponseItem>> = repository.counties
+    var subCounties: MutableState<List<AllSubCountiesResponseItem>> = repository.subCounties
 
     fun getClinic(name: String){
         viewModelScope.launch {
@@ -31,10 +35,23 @@ class ClinicsViewModel @Inject constructor(
         }
     }
 
+    fun getSubCounties(id: Int){
+        viewModelScope.launch {
+            repository.getSubCounties(id)
+        }
+    }
+
+    fun getAllClinicsInALocation(location: Location){
+        viewModelScope.launch {
+            repository.getAllClinicsInALocation(location)
+        }
+    }
+
 
     init {
        viewModelScope.launch {
               repository.getAllClinics()
+           repository.getCountriesAndCounties()
        }
     }
 
