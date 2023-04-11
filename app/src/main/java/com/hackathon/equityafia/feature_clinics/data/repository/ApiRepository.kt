35759d7f757c47abd64725c4d9be0d74f9727ef5
Terry Mochire <terrybmochire@gmail.com>
2023-04-11 04:55:28 +0300

    Refactor UI and navigate to LocationsFormScreen.kt

--- app/src/main/java/com/hackathon/equityafia/feature_clinics/data/repository/ApiRepository.kt
+++ app/src/main/java/com/hackathon/equityafia/feature_clinics/data/repository/ApiRepository.kt
@@ -3,14 +3,17 @@ package com.hackathon.equityafia.feature_clinics.data.repository
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import com.hackathon.equityafia.feature_clinics.data.remote.api.ApiService
-import com.hackathon.equityafia.feature_clinics.data.remote.models.responses.AllClinicsResponseItem
-import com.hackathon.equityafia.feature_clinics.data.remote.models.responses.AllServicesResponseItem
+import com.hackathon.equityafia.feature_clinics.data.remote.models.requests.Location
+import com.hackathon.equityafia.feature_clinics.data.remote.models.responses.*
 import javax.inject.Inject
 
 class ApiRepository @Inject constructor(private val apiService: ApiService) {
 
     val clinics: MutableState<List<AllClinicsResponseItem>> = mutableStateOf(emptyList())
     val services: MutableState<List<AllServicesResponseItem>> = mutableStateOf(emptyList())
+    val countries: MutableState<List<AllCountriesResponseItem>> = mutableStateOf(emptyList())
+    val counties: MutableState<List<AllCountiesResponseItem>> = mutableStateOf(emptyList())
+    val subCounties: MutableState<List<AllSubCountiesResponseItem>> = mutableStateOf(emptyList())
 
     private var token:String? = null
 
@@ -35,6 +38,24 @@ class ApiRepository @Inject constructor(private val apiService: ApiService) {
     suspend fun getSerivicesInAClinic(id: Int) {
        val response = apiService.getAllServicesInAClinic(id)
         services.value = response
+    }
+
+    suspend fun getCountriesAndCounties() {
+       val countries = apiService.getAllCountries()
+        val counties = apiService.getAllCounties()
+        this.countries.value = countries
+        this.counties.value = counties
+    }
+
+    suspend fun getSubCounties(id: Int) {
+       val subCounties = apiService.getAllSubCountiesInACounty(id)
+        this.subCounties.value = subCounties
+    }
+
+
+    suspend fun getAllClinicsInALocation(location: Location){
+        val response = apiService.getClinicByLocation(getToken(), location)
+        clinics.value = response
     }
 
 
