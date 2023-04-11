cd653e58b2adb4afba299e9c4b9e65b4454043ae
Terry Mochire <terrybmochire@gmail.com>
2023-04-11 03:24:09 +0300

    Implement function to retrieve the services in a clinic.

--- app/src/main/java/com/hackathon/equityafia/feature_clinics/presentation/viewmodels/ClinicsViewModel.kt
+++ app/src/main/java/com/hackathon/equityafia/feature_clinics/presentation/viewmodels/ClinicsViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.MutableState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hackathon.equityafia.feature_clinics.data.remote.models.responses.AllClinicsResponseItem
+import com.hackathon.equityafia.feature_clinics.data.remote.models.responses.AllServicesResponseItem
 import com.hackathon.equityafia.feature_clinics.data.repository.ApiRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -16,12 +17,20 @@ class ClinicsViewModel @Inject constructor(
 ) : ViewModel() {
 
     var clinics: MutableState<List<AllClinicsResponseItem>> = repository.clinics
+    var services: MutableState<List<AllServicesResponseItem>> = repository.services
 
     fun getClinic(name: String){
         viewModelScope.launch {
              repository.getClinic(name)
         }
     }
+
+    fun getServicesInAClinic(id: Int){
+        viewModelScope.launch {
+             repository.getSerivicesInAClinic(id)
+        }
+    }
+
 
     init {
        viewModelScope.launch {
